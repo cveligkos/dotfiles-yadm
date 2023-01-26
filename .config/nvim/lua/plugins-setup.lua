9f56f0e99930b8dad065f0780310f90e80c35e97
Christos Veligkos <cveligkos@gmail.com>
2023-01-26 08:34:45 +0200

    Neovim noice config

--- .config/nvim/lua/plugins-setup.lua
+++ .config/nvim/lua/plugins-setup.lua
@@ -98,6 +98,18 @@ return packer.startup(function(use)
 	use("folke/twilight.nvim")
 	use("folke/zen-mode.nvim")
 
+	use({
+		"folke/noice.nvim",
+		requires = {
+			-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+			"MunifTanjim/nui.nvim",
+			-- OPTIONAL:
+			--   `nvim-notify` is only needed, if you want to use the notification view.
+			--   If not available, we use `mini` as the fallback
+			"rcarriga/nvim-notify",
+		},
+	})
+
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
 	if packer_bootstrap then
--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -24,6 +24,7 @@ treesitter.setup({
 		"html",
 		"svelte",
 		"python",
+		"markdown",
 	},
 	auto_install = true,
 })
